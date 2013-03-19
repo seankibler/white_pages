b8e4b4a5e8940aaa355b15e032726ae21b694d58
Sean Kibler <skibler1223@gmail.com>
2013-03-19 19:28:25 -0400

    Client does not need to be initialized with API key and it causes argument errors.

--- lib/white_pages.rb
+++ lib/white_pages.rb
@@ -10,18 +10,18 @@ module WhitePages
   end
   
   def self.find_person(options)
-    Client.new(self.api_key).find_person(options)
+    Client.new.find_person(options)
   end
   
   def self.find_business(options)
-    Client.new(self.api_key).find_business(options)
+    Client.new.find_business(options)
   end
   
   def self.reverse_address(options)
-    Client.new(self.api_key).reverse_address(options)
+    Client.new.reverse_address(options)
   end
   
   def self.reverse_phone(options)
-    Client.new(self.api_key).reverse_phone(options)
+    Client.new.reverse_phone(options)
   end
 end
